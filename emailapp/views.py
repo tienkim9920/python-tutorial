a9e6b2f19c68da418d952171666fdff6cc01ff79
Tien Kim <tienkim9920@gmail.com>
2025-05-11 23:58:13 +0700

    update

--- emailapp/views.py
+++ emailapp/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .email import get_Email_list
 
 def home(request):
-  emails = get_Email_list()
+  emails = [
+    {
+      'title': 'Thong bao lich phong van',
+      'description': 'Phong van luc 14h00',
+      'time': '04/05/2025',
+      'status': False,
+    },
+    {
+      'title': 'Ket qua phong van',
+      'description': 'Chuc mung ban',
+      'time': '04/05/2025',
+      'status': True,
+    },
+    {
+      'title': 'Thong bao lich di lam viec',
+      'description': 'Thu 2 den thu 6',
+      'time': '04/05/2025',
+      'status': True,
+    },
+  ]
   return render(request, 'home.html', { 'emails': emails })
